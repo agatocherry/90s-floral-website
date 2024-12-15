6ae592b4a5fe003948447f730d23b0c173d333c4
agatocherry <agatocherry@gmail.com>
2024-12-15 13:57:13 +0100

    Update the `js` file

--- analytics.js
+++ analytics.js
@@ -1,4 +1,7 @@
 import { inject } from '@vercel/analytics';
-
-// Initialise les analytics Vercel
-inject();
+// import some helper that is exposed by your current framework to determine the right mode manually
+import { dev } from '$app/environment';
+ 
+inject({
+  mode: dev ? 'development' : 'production',
+});
